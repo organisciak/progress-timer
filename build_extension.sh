35f9750bb7ad925122be4adaf1560b1aa99baffe
Peter Organisciak <organis2@illinois.edu>
2012-12-31 00:35:49 -0600

    Build script now moves css images

--- build_extension.sh
+++ build_extension.sh
@@ -3,6 +3,7 @@
 ### CHROME EXTENSION BUILD SCRIPT ###
 # Requirements:
 # * Closure Compiler - Download from https://docs.google.com/viewer?url=http%3A%2F%2Fclosure-compiler.googlecode.com%2Ffiles%2Fcompiler-latest.zip and extract full folder to lib/
+# * SASS - http://sass-lang.com/
 
 #Exit Shell Script on fail
 set -e
@@ -12,6 +13,7 @@ echo "Checking for errors and compiling progress.js..."
 mkdir -p extension/js/
 #Set progress.js debugging flag to false
 sed -i.bak "s/debug\: true/debug\: false/g" js/progress.js
+rm js/progress.js.bak
 
 #Array of all javascript files to compile
 javascripts=(js/lib/jquery-1.8.2.min.js js/lib/jquery-ui.js js/lib/d3.v2.min.js js/lib/json2.js js/delta/modernizr-2.0.6.min.js js/globalize-master/lib/globalize.js js/lib/jquery-ui-timepicker-addon.js js/delta/custom.js js/progress.js)
@@ -20,6 +22,7 @@ commands=$(for file in "${javascripts[@]}"; do echo "--js $file"; done)
 java -jar lib/compiler-latest/compiler.jar --js_output_file extension/js/progress.js $commands
 
 ## 2. Copy HTML to extension folder and modify to use compiles JS
+echo "Preparing HTML..."
 cp progress.html extension/progress.html
 #Remove <script> tags for all compiled JS (except js/progress.js)
 for file in "${javascripts[@]}"; do 
@@ -27,14 +30,20 @@ for file in "${javascripts[@]}"; do
 	  sed -i.bak "s:^.*<script.*src=\"$file\".*::g" extension/progress.html;
 	fi
 done
+rm extension/progress.html.bak
 
-## 2. Compile SCSS to compress CSS
-echo "Compiling SCSS..."
-mkdir -p extension/css
+## 3. Compile SCSS to compress CSS
+echo "Compiling SCSS to CSS..."
+mkdir -p extension/css/images
 sass --style compressed dev/style.scss:extension/css/style.css
+
+# Move CSS Files to extension folder
+rm -rf extension/css/images/*
+cp -r css/images/* extension/css/images
 
 #Set progress.js debugging flag back to true
 sed -i.bak "s/debug\: false/debug\: true/g" js/progress.js
 
+echo "Chrome extension compiled successfully."
 exit 0
 
