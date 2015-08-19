aff5e70e2f5d4fe3d7b593f3a1b18da8d061a532
Peter Organisciak <organisciak@gmail.com>
2015-08-19 16:00:00 -0600

    Inital app commits

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name progressTimerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the progressTimerApp
+ */
+angular.module('progressTimerApp')
+  .controller('MainCtrl', ['$http', '$log', '$scope', 
+  function ($http, $log, $scope) {
+    var loadData;
+    var main = this;
+
+    loadData = $http.get('diss-export.json');
+
+    loadData.success(function(data) {
+      main.data = data;
+      $scope.bars = data.bars;
+
+    });
+
+    loadData.error(function(data, status, headers, config) {
+      console.error(status);
+    });
+
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
