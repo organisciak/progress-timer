3d29a12e7f9be5a19d476f0dfcdd8a79c0ee5cc1
Peter Organisciak <organisciak@gmail.com>
2015-08-25 11:17:09 -0600

    Calculate bar percentage

--- app/scripts/controllers/bar.js
+++ app/scripts/controllers/bar.js
@@ -13,19 +13,56 @@ angular.module('progressTimerApp')
       // perhaps this.controller can be used to import
       // type-specific controller elements
 
-      // Update the progress percentile info
-      var updatePercentile = function(bar) {
-        if ($scope.bar.type == 'timer') {
-          $scope.bar.percentile = 60;
-        } else if ($scope.bar.type == 'counter') {
-          $scope.bar.percentile = 100*(($scope.bar.current-$scope.bar.start)/
-              ($scope.bar.end-$scope.bar.start));
-        } else if ($scope.bar.type == 'clock') {
-          $scope.bar.percentile = 10;
+      // Update schema, in case of older data
+      if (typeof $scope.bar.running === 'undefined') {
+        if ($scope.bar.type === 'timer' && $scope.bar.progress.start) {
+          $scope.bar.running = true;
+        } else if ($scope.bar.type === 'clock') {
+          $scope.bar.running = true;
         } else {
-          $log.error("No recognized type for percentile calculation.")
+          $scope.bar.running = false;
         }
+      }
+
+      // Update the progress percentile info
+      var updatePercentile = function() {
+        var percentile;
+        var bar = $scope.bar;
+        var current = (function() {
+            if (bar.type === 'timer' && bar.progress.start) {
+                return bar.current +
+                    new Date().getTime() - bar.progress.start;
+            } else {
+                return bar.current;
+            }
+        })();
+        
+        var countUp = (bar.start < bar.end);
+
+        if (current <= bar.start && countUp) {
+           percentile = 0;
+        }
+        else if (current >= bar.end && countUp) {
+            percentile = 100;
+        }
+        else if (current <= bar.end && !countUp) {
+            percentile = 100;
+        }
+        else if (current >= bar.start && !countUp) {
+            percentile = 0;
+        } else {
+            percentile = countUp ?
+                100 * (current - bar.start) / (bar.end - bar.start) :
+                100 * (bar.start - current) / (bar.start - bar.end);
+        }
+        if (percentile >= 100) {
+          $scope.bar.running = false;
+        }
+        return $scope.bar.percentile = percentile;
+        //$scope.bar.running = true;
       };
+
+      //$scope.bar.running = true;
 
       updatePercentile();
 
